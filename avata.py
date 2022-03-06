4879817176fdcc366b7068ab00e57ae467a6d997
Shaun <614027488@qq.com>
2022-03-07 03:21:17 +0800

    create room.html

--- avata.py
+++ avata.py
@@ -1,6 +1,7 @@
 import os
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask import Flask
+from easydict import EasyDict
 
 PATH_templates='frontend/templates'
 PATH_static='frontend/static'
