1e605f77df48c3c9e14025db2a0345c92e93d6d4
Shaun <614027488@qq.com>
2022-03-09 03:52:27 +0800

    Update avata.py

--- avata.py
+++ avata.py
@@ -1,3 +1,4 @@
+from fileinput import filename
 import os
 from flask import Blueprint, render_template, redirect, url_for, request
 from flask import Flask
@@ -13,39 +14,40 @@ app = Flask(__name__, template_folder=PATH_templates, static_folder=PATH_static)
 def hello():
     return 'hello'
 
-H=1080
-W=1920
-dic={#1080*1920
-    0:{
-        'name':'d1',
-        'v':str(int(H*0.2))+'px',
-        'u':str(int(W*0.2))+'px'
-    },
-    1:{
-        'name':'d2',
-        'v':str(int(H*0.8))+'px',
-        'u':str(int(W*0.3))+'px'
-    },
-    2:{
-        'name':'d3',
-        'v':str(int(H*0.1))+'px',
-        'u':str(int(W*0.6))+'px'
+def demo_dic(H=1080,W=1920):
+    dic={#1080*1920
+        0:{
+            'name':'d1',
+            'v':str(int(H*0.2))+'px',
+            'u':str(int(W*0.2))+'px'
+        },
+        1:{
+            'name':'d2',
+            'v':str(int(H*0.8))+'px',
+            'u':str(int(W*0.3))+'px'
+        },
+        2:{
+            'name':'d3',
+            'v':str(int(H*0.1))+'px',
+            'u':str(int(W*0.6))+'px'
+        }
     }
-}
+    return dic
 
 @app.route("/device", methods=['POST','GET'])
 def demo():
     if request.method == "POST":
         confirm=request.form.get('confirm')
         device={}
-        for i, d in dic.items():
+        dic_=demo_dic()
+        for i, d in dic_.items():
             if request.form.get(d['name']):
                 device[i]=request.form.get(d['name'])
             else: device[i]=''
 
         return device
 
-    return render_template('device.html',dic=dic)
+    return render_template('device.html',dic=demo_dic())
 
 @app.route("/search", methods=['POST','GET'])
 def search():
@@ -55,6 +57,16 @@ def search():
         return room_id
 
     return render_template('search.html',room_id=room_id)
+
+@app.route("/instruction-choose", methods=['POST','GET'])
+def instructor_choose():
+    if request.method == "POST":
+        name=request.form.get('input')
+        return name
+    
+    image_path=url_for('static',filename='images/grey.jpg')
+
+    return render_template('instruction-choose.html',dic=demo_dic(300,300), image_path=image_path)
 
 
 if __name__ == "__main__":
