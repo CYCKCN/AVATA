3c6cd276a0b723f0ce237adf48d3dfb9737f4883
Shaun <614027488@qq.com>
2022-03-11 21:49:21 +0800

    Update demo-create-json.py

--- demo-create-json.py
+++ demo-create-json.py
@@ -6,7 +6,7 @@ import img_trans
 
 #img_trans.image_encoder('')
 room = {
-    'id':'4223',
+    'id':'IEDA Conference Room',
     'image-room':img_trans.image_encoder('frontend\static\images\demo-ieda\\360-2.JPG'),
     'image-360':img_trans.image_encoder('frontend\static\images\demo-ieda\\360-1.JPG'),
     'mac':{
@@ -21,7 +21,8 @@ room = {
                 0:'Find the control box on the lectern and press “System On”',
                 1:'Select as shown in the picture (the projector needs time to be ready, please wait)',
                 2:'Continue to set up at your Apple laptop'
-            }
+            },
+            'type':'.jpg'
         },
         1:{
             'title':'Apple',
@@ -32,7 +33,8 @@ room = {
             'text':{
                 0:'Select “Screen Mirroring” - “ielm conference”',
                 1:'Enter the AirPlay code'
-            }
+            },
+            'type':'.mp4'
         }
     },
     'win':{
@@ -47,7 +49,8 @@ room = {
                 0:'Find the control box on the lectern and press “System On”',
                 1:'Select as shown in the picture (the projector needs time to be ready, please wait)',
                 2:'Continue to set up at the conference table'
-            }
+            },
+            'type':'.jpg'
         },
         1:{
             'title':'Conference Table',
@@ -62,7 +65,8 @@ room = {
                 1:'Plug the HDMI cable into your laptop',
                 2:'Press “Fn” and “F7” on your laptop',
                 3:'Select “Duplicate” on the pop-up window'
-            }
+            },
+            'type':'.jpg'
         }
     },
     'none':{
@@ -77,12 +81,13 @@ room = {
                 0:'Find the control box on the lectern and press “System On”',
                 1:'Select as shown in the picture (the projector needs time to be ready, please wait)',
                 2:'The devices are ready for your presentation'
-            }
+            },
+            'type':'.jpg'
         },
     }
 }
 
-out = open('data.json','w')
+out = open('demo-data.json','w')
 json.dump(room,out)
 
 
