d1e19247e5ca86941923c69357c756233d38b074
Shaun <614027488@qq.com>
2022-04-04 01:52:29 +0800

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -1,7 +1,8 @@
 import os
+import time
 from pprint import pprint
-from posixpath import devnull
-from queue import Queue
+
+from cv2 import line
 from flask import Blueprint, render_template, redirect, url_for, request
 
 class ROOM:
@@ -26,15 +27,21 @@ class ROOM:
         f = open(path)
         for line in f:
             lines.append(line.replace('\n',''))
+        f.close()
         return lines
 
-    def __read_guide_txt(self,path):
-        lines=[]
-        f = open(path)
-        for line in f:
-            if line.split('_')[0] in self.choose:
-                lines.append(line.replace('\n',''))
-        return lines
+    def __read_guide_txt(self):
+        L={}
+        for p in self.choose_personal:
+            root=os.path.join(self.ROOT,p,'text.txt')
+            lines=[]
+            f = open(root)
+            for line in f:
+                if line.split('_')[0] in self.choose:
+                    lines.append(line.replace('\n',''))
+            f.close()
+            L[p]=lines
+        return L
 
     def set_data_choose_devices(self):#database->devices.html
         root=os.path.join(self.ROOT,'360.txt')
@@ -67,39 +74,71 @@ class ROOM:
         return choose_devices
 
     def set_data_instruction(self):#database->instruction(-choose).html
-        root=os.path.join(self.ROOT,self.choose_personal,'text.txt')
-        lines=self.__read_guide_txt(root)
+        #root=os.path.join(self.ROOT,self.choose_personal,'text.txt')
+        #lines=self.__read_guide_txt()
         #print(lines)
+        L=self.__read_guide_txt()
+        #pprint(L)
         guides={}
-        pos_line=0
+        #pos_line=0
         for i in range(len(self.choose)):
-            pos_guide=0
             guide={}
-            g={}
-            for j in range(pos_line,len(lines)):
-                device_name=lines[j].split('_')[0]
-                guide_name=lines[j].split('_')[1]
-                image=lines[j].split(' ')[0]
-                text=lines[j].split(' ')[1]
-                if device_name == self.choose[i]:
-                    if not guide_name == 'guide'+str(pos_guide+1):
-                        pos_guide+=1
-                        guide[len(guide)]={
-                            'finish':0,
-                            'guide':g.copy()
-                        }
-                        g={}
-                    g[text]=url_for('static',
-                    filename='images/test/'+self.room_id+'/'+self.choose_personal+'/'+image+'.png')\
-                        if not self.__debug else 'images/test/'+self.room_id+'/'+self.choose_personal+'/'+image+'.png'
+            for p, lines in L.items():
+                '''
+                pos_guide=0
+                g={}
 
-                else: 
-                    pos_line=j
-                    break
-            guide[len(guide)]={
-                'finish':0,
-                'guide':g.copy()
-            }
+                
+                for j in range(pos_line,len(lines)):
+                    device_name=lines[j].split('_')[0]
+                    guide_name=lines[j].split('_')[1]
+                    image=lines[j].split(' ')[0]
+                    text=lines[j].split(' ')[1]
+                    if device_name == self.choose[i]:
+                        if not guide_name == 'guide'+str(pos_guide+1):
+                            pos_guide+=1
+                            pprint(g)
+                            guide[len(guide)]={
+                                'finish':0,
+                                'guide':g.copy()
+                            }
+                            g={}
+                        g[text]=url_for('static',
+                        filename='images/test/'+self.room_id+'/'+p+'/'+image+'.png')\
+                            if not self.__debug else 'images/test/'+self.room_id+'/'+p+'/'+image+'.png'
+
+                    else: 
+                        print(device_name)
+                        pos_line=j
+                        break
+                pprint(g)
+                guide[len(guide)]={
+                    'finish':0,
+                    'guide':g.copy()
+                }
+                '''
+                cur_device_guide=[]
+                for j in range(len(lines)):
+                    device_name=lines[j].split('_')[0]
+                    if device_name==self.choose[i]:
+                        cur_device_guide.append(lines[j])
+                step_num=int(cur_device_guide[-1].split('_')[1].replace('guide',''))
+
+                for j in range(step_num):
+                    g={}
+                    for k in range(len(cur_device_guide)):
+                        cur_name=int(cur_device_guide[k].split('_')[1].replace('guide',''))
+                        if cur_name==j+1:
+                            image=cur_device_guide[k].split(' ')[0]
+                            text=cur_device_guide[k].split(' ')[1]
+                            g[text]=url_for('static',
+                            filename='images/test/'+self.room_id+'/'+p+'/'+image+'.png')\
+                                if not self.__debug else 'images/test/'+self.room_id+'/'+p+'/'+image+'.png'
+                    guide[len(guide)]={
+                        'finish':0,
+                        'guide':g.copy()
+                    }
+
             guides[self.choose[i]]=guide.copy()
         self.guides=guides
         return guides
@@ -143,9 +182,13 @@ if __name__ == '__main__':
     devices=room.set_data_choose_devices()
     #pprint(devices)
     room.choose=['device001','device002','device004']
-    choose_devices=room.get_data_personal_device('win')
+    choose_devices=room.get_data_personal_device(['win','mac'])
     #pprint(choose_devices)
+    _s=time.time()
     guides=room.set_data_instruction()
+    _e=time.time()-_s
     pprint(guides)
+    print(_e)
+
 
     
