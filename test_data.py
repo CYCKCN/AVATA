1eb82b49701a221caf2210ed8c30f1f8f155e2e2
Shaun <614027488@qq.com>
2022-04-25 03:12:52 +0800

    Update test_data.py

--- test_data.py
+++ test_data.py
@@ -27,13 +27,13 @@ MONTH_ABBR={
     12:'Dec'
 }
 WEEK_ABBR={
-    1:'Mon',
-    2:'Tue',
-    3:'Wed',
-    4:'Thu',
-    5:'Fri',
-    6:'Sat',
-    7:'Sun'
+    0:'Mon',
+    1:'Tue',
+    2:'Wed',
+    3:'Thu',
+    4:'Fri',
+    5:'Sat',
+    6:'Sun'
 }
 
 class ROOM:
