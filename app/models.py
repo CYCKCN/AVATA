053dd7ea9b0c47b54ab59d6674a69d7613729717
CYCKCN <cyckcn@163.com>
2022-06-20 09:28:45 +0800

    dbfinished

--- app/models.py
+++ app/models.py
@@ -72,21 +72,24 @@ class Account(object):
         self.accountID = identity # "USER" / "ADMIN"
 
 class Device(object):
-    def __init__(self, roomName, deviceName, deviceType, deviceIP, deviceLoc):
-        self.roomName = roomName # ["IEDA Conference Room", "5554"]
+    def __init__(self, deviceID, roomName, deviceName, deviceType, deviceIP, deviceLocX, deviceLocY):
+        self.deviceID = deviceID
+        self.roomName = roomName # "IEDA Conference Room, Room 5554"
         self.deviceName = deviceName # "project_1"
         self.deviceType = deviceType # "display_projector_WIFI"
         self.deviceIP = deviceIP # "000.00.000.000:0000"
-        self.deviceLoc = deviceLoc # [0, 0]
+        self.deviceLocX = deviceLocX # 1
+        self.deviceLocY = deviceLocY # 2
 
 class Room(object):
-    def __init__(self, roomName, roomImg, roomLoc, bookBy, bookTime, insInitial, insTurnon, insPair, insZoom):
-        self.roomName = roomName
-        self.roomImg = roomImg
-        self.roomLoc = roomLoc
-        self.bookBy = bookBy
-        self.bookTime = bookTime
-        self.insInitial = insInitial
-        self.insTurnon = insTurnon
-        self.insPair = insPair
-        self.insZoom = insZoom
+    def __init__(self, roomName, roomImg, roomLoc):
+        self.roomName = roomName # "IEDA Conference Room, Room 5554"
+        self.roomImg = roomImg # ""
+        self.roomLoc = roomLoc # "Academic Building"
+        self.room360Img = ""
+        self.bookBy = {}
+        self.bookTime = {}
+        self.insInitial = []
+        self.insTurnon = {}
+        self.insPair = []
+        self.insZoom = {"video": [], "audio": []}
